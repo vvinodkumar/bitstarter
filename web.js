626a83ad8417093299a5c70a8dbbb87b6a4971dc
Vinod Kumar <vinodkumar.vasudevan@gmail.com>
2013-07-10 13:50:16 +0000

    Updated web.js again

--- web.js
+++ web.js
@@ -5,13 +5,13 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 
-var htmlData = '';
+
 fs.readFileSync('index.html', 'utf8',function (err, data) {
   if (err) throw err;
-  htmlData = data;
+ response.send(data);
 });
 
-response.send(htmlData);
+
 });
 
 var port = process.env.PORT || 5000;
